5144e1e778b3030d14bdefc1cac8ca0ce9556dc8
Martin Zerbib <martinzerbib@gmail.com>
2014-07-20 11:41:43 -0700

    pubnub

--- app/controllers/random.js
+++ app/controllers/random.js
@@ -13,11 +13,22 @@ default Ember.ObjectController.extend({
     actions:{
         choice:function(val){
             if (val===1) 
-            {
+            {   
+                var self = this;
                 var conv_id=this.get('controllers.application.id')+"+"+this.get('id');
                 Ember.$.get('http://123.harveychan.net:3000/addtoqueue_a/'+this.get('controllers.application.id')+"/"+this.get('id'));
                 Ember.$.get('http://123.harveychan.net:3000/addtoqueue_b/'+this.get('id')+'/'+this.get('controllers.application.id'));
+                Ember.$.get('http://123.harveychan.net:3000/user/'+this.get('controllers.application.id'),function(data){
+                data=JSON.parse(data);
+                if (data.matches.length>0){
+                   self.get('pubnub').publish(self.get('id'),{'match':true,'text':'','name':self.get('name')});
+                  // self.transitionTo('lunch');
+                }
+                else{
+                   self.get('pubnub').publish(self.get('id'),{'invite':true,'text':'','name':self.get('name')});
+                }
 
+                });
 //                this.get('pubnub').conversationsubscribe(conv_id,this);
                 //conversation.save();
                 //this.transitionTo('conversation',conversation);
